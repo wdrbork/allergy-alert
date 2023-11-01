32020a5c3cd2dac98be4e334dbc257f2d7c6b257
wdrbork <wdrb2001@gmail.com>
2023-10-31 20:51:03 -0700

    Merge package.json files and move tests to root

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,13 +1,15 @@
 import express from "express"
 import cors from "cors"
 import recipes from "./api/recipes.route.js"
+import hello from "./api/hello.route.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use("/", recipes)
+app.use("/", hello)
+app.use("/api/v1/recipes", recipes)
 app.use("*", (req, res) => res.status(404).json({error: "Not Found"}))
 
 export default app
--- test/backend/api_tests.js
+++ test/backend/api_tests.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import request from 'supertest';
 
-import app from '../server.js';
+import app from '../../src/backend/server.js';
 
 // follow this format for adding a test for the api
 describe('Hello World Route', () => {
