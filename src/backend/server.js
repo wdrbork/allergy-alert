89ad49692e000c15d492a2c591a933c63002aa36
wdrbork <wdrb2001@gmail.com>
2023-10-31 20:19:39 -0700

    Revert "Move package.json file to directory root and adjust URL for 'recipes' call"
    This reverts commit b0ca5b31436f5dfee4aa64a584795155c3980b1d.

--- src/backend/server.js
+++ src/backend/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/v1/recipes", recipes)
+app.use("/", recipes)
 app.use("*", (req, res) => res.status(404).json({error: "Not Found"}))
 
 export default app
