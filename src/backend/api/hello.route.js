6e7a26fdb9646a1c066b93f4aa945d31393dddc6
Caitlyn Rawlings <crawlin@cs.washington.edu>
2023-11-01 09:13:37 -0700

    adjusted test; added start script

--- src/backend/api/hello.route.js
+++ src/backend/api/hello.route.js
@@ -4,6 +4,8 @@ import express from "express"
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send('Hello World'))
+router.get('/test', (req, res) => {
+    res.send("Hello World")
+}) 
 
 export default router
