45d46a370a2a92e94b90aebb28444bae064c6a6c
wdrbork <wdrb2001@gmail.com>
2023-11-02 15:49:04 -0700

    Add instructions for searching for a specific recipe

--- src/backend/api/recipes.route.js
+++ src/backend/api/recipes.route.js
@@ -3,6 +3,7 @@ import RecipesCtrl from "./recipes.controller.js"
 
 const router = express.Router()
 
+// To search for a specific recipe, search for "localhost:5000/api/v1/recipes?name=<recipe_name>"
 router.route("/").get(RecipesCtrl.apiGetRecipes)
 
 export default router
