021719a8279c2825e3be537eae1744e607a01fd4
Riley Bork <wdrb2001@cs.washington.edu>
2023-11-02 14:19:30 -0700

    Add instructions to server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,6 +8,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// To add a call to the database, include the call "app.use(<URL from root>)"
+// and add a "route" file to the API folder that follows the existing layout
 app.use("/api/v1/hello", hello)  // This is purely for testing
 app.use("/api/v1/recipes", recipes)
 app.use("*", (req, res) => res.status(404).json({error: "Not Found"}))
