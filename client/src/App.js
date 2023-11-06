75ee3eb038f6651f54b197bf9ac45f88eb2ef784
Jack Rosenbloom <76887594+jackcodesstuff@users.noreply.github.com>
2023-11-05 17:46:49 -0800

    Update App.js
    changed to have recent update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
+import SearchBar from './SearchBar';
+import AllergyList from './AllergyList';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>Hello worl;d</div>
+        <h2 className="logo-text">Allergy Alert</h2>
       </header>
+      <main>
+        <section>
+          <h3>About Allergy Alert</h3>
+          <p>
+            Allergy Alert allows users to quickly find recipes and foods which contain their allergies and which don't,
+            allowing for faster access to appropriate foods to enjoy anytime, anywhere.
+          </p>
+        </section>
+        <section>
+          <AllergyList/>
+          <SearchBar placeholder="🔍 Recipe" onSearch={() => { } } />
+        </section>
+      </main>
     </div>
   );
 }
