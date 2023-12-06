6b80118800e1913ea80d8319be1da304e3bf1e80
crawlin <crawlin@uw.edu>
2023-12-05 20:47:53 -0800

    added frontend tests

--- client/src/app.test.js
+++ client/src/app.test.js
@@ -2,8 +2,13 @@
   Unit tests for the App component using the testing-library/react library.
 */
 
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, waitFor, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NavBar from './NavBar';
 import App from './App';
+import SearchBar from './SearchBar';
+
 
 test('test', () => {
   render(<App />);
@@ -12,32 +17,48 @@ test('test', () => {
 
 describe('Renders starter screen', () => {
 
-    test('renders Allergy Alert logo', () => {
+  test('renders Allergy Alert logo', () => {
     render(<App />);
-    const logoElement = screen.getByText(/Allergy Alert/, { selector: 'h2.logo-text' });
+    const logoElement = screen.getByText(/Allergy Alert/, { selector: '.logo-text' });
     expect(logoElement).toBeDefined();
-    });
+  });
 
-    test('renders About Allergy Alert section', () => {
-    const { getByText } = render(<App />);
-    const aboutSection = getByText((content, element) => {
-        return element.tagName.toLowerCase() === 'h3' && content.includes('About Allergy Alert');
-    });
-    expect(aboutSection).toBeDefined();
-    });
+  test('renders the NavBar and triggers Manage Allergies', () => {
+    // Mock function for the emitDisplayAllergyListIntent prop
+    const mockToggleAllergyList = jest.fn();
+  
+    // Render the NavBar with the mock function
+    render(<NavBar emitDisplayAllergyListIntent={mockToggleAllergyList} />);
+  
+    // Check if the logo text is rendered
+    const logoText = screen.getByText('Allergy Alert');
+    expect(logoText).toBeDefined();
+  
+    // Check if the "About" button is rendered
+    const aboutButton = screen.getByText('About');
+    expect(aboutButton).toBeDefined();
+  
+    // Click on the "Manage Allergies" button using userEvent
+    const manageAllergiesButton = screen.getByText('Manage Allergies');
+    userEvent.click(manageAllergiesButton);
+  
+    // Check if the mock function was called
+    expect(mockToggleAllergyList).toHaveBeenCalledTimes(1);
+  });
 
-    test('renders About Allergy Alert info section', () => {
-    const { getByText } = render(<App />);
-    const aboutAllergyAlertInfo = getByText((content, element) => {
-        return element.tagName.toLowerCase() === 'p' && content.includes("Allergy Alert allows users to "
-                                                                        + "quickly find recipes and foods "
-                                                                        + "which contain their allergies and "
-                                                                        + "which don't, allowing for faster "
-                                                                        + "access to appropriate foods to enjoy "
-                                                                        + "anytime, anywhere.");
-    });
-    expect(aboutAllergyAlertInfo).toBeDefined();
-    });
+  // Mock function for the emitSearchIntent prop
+  const mockSearchIntent = jest.fn();
+
+  test('renders SearchBar component with placeholder', () => {
+    render(<SearchBar placeholder="🔍 Recipe" emitSearchIntent={mockSearchIntent} />);
+
+    // Check if the input element and button are rendered
+    const inputElement = screen.getByPlaceholderText('🔍 Recipe');
+    const buttonElement = screen.getByText('▶');
+
+    expect(inputElement).toBeTruthy();
+    expect(buttonElement).toBeTruthy();
+  });
 
 });
 
